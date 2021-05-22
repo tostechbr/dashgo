d8cbf3e7b34dd8b935243c4ac07e04d15b2c2efc
Tiago Oliveira <tiagolivesan@outlook.com>
2021-05-22 12:34:37 -0300

    dashgo

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
 import { Header } from "../../components/Header";
@@ -6,6 +6,11 @@ import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true,
+    })
+
     return (
         <Box>
             <Header />
@@ -31,17 +36,17 @@ export default function UserList() {
                     <Table colorScheme="whiteAlpha">
                         <Thead>
                             <Tr>
-                                <Th px="6" color="gray.300" width="8">
+                                <Th px={["4", "4", "6"]} color="gray.300" width="8">
                                     <Checkbox colorScheme="pink"/>
                                 </Th>
                                 <Th>Usário</Th>
-                                <Th>Data de cadastro</Th>
+                                { isWideVersion && <Th>Data de cadastro</Th> }
                                 <Th width="8"></Th>
                             </Tr>
                         </Thead>
                         <Tbody>
                             <Tr>
-                                <Td px="6">
+                                <Td px={["4", "4", "6"]}>
                                     <Checkbox colorScheme="pink"/>
                                 </Td>
                                 <Td>
@@ -50,20 +55,20 @@ export default function UserList() {
                                         <Text fontSize="sm" color="gray.300">tiagolivesan@outlook.com</Text>
                                     </Box>
                                 </Td>
-                                <Td>15 de Maio, 2021</Td>
+                                { isWideVersion && <Td>15 de Maio, 2021</Td>}
                                 <Td>
-                                <Button
-                                    as="a"
-                                    size="sm"
-                                    fontSize="sm"
-                                    colorScheme="blue"
-                                    leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}>
-                                        Editar
-                                </Button>
+                                    <Button
+                                        as="a"
+                                        size="sm"
+                                        fontSize="sm"
+                                        colorScheme="blue"
+                                        leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}>
+                                            Editar
+                                    </Button>
                                 </Td>
                             </Tr>
                             <Tr>
-                                <Td px="6">
+                                <Td px={["4", "4", "6"]}>
                                     <Checkbox colorScheme="pink"/>
                                 </Td>
                                 <Td>
@@ -72,7 +77,7 @@ export default function UserList() {
                                         <Text fontSize="sm" color="gray.300">ana-csm@outlook.com</Text>
                                     </Box>
                                 </Td>
-                                <Td>15 de Maio, 2021</Td>
+                                { isWideVersion && <Td>15 de Maio, 2021</Td>}
                                 <Td>
                                 <Button
                                     as="a"
