841514821a96cc6f27832f7d6569270dd01aebeb
Tiago Oliveira <tiagolivesan@outlook.com>
2021-05-29 20:12:44 -0300

    dashgo

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,36 +16,16 @@ import {
   Spinner,
 } from "@chakra-ui/react";
 import Link from "next/link";
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { RiAddLine } from "react-icons/ri";
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
-import { useQuery } from "react-query";
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
-  const { data, isLoading, error, isFetching } = useQuery("users", async () => {
-    const response = await fetch("http://localhost:3000/api/users");
-    const data = await response.json();
-
-    const users = data.users.map((user) => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
-          day: "2-digit",
-          month: "long",
-          year: "numeric",
-        }),
-      };
-    });
-
-    return users;
-  }, {
-    staleTime: 1000 * 5,
-  });
+  const { data, isLoading, error, isFetching } = useUsers()
 
   console.log(data);
 
