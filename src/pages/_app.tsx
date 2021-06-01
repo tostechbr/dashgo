cc29613b694cef380018e08fd93efe65435b5ef0
Tiago Oliveira <tiagolivesan@outlook.com>
2021-06-01 11:11:03 -0300

    dashgo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,14 +5,15 @@ import { ReactQueryDevtools } from 'react-query/devtools'
 
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext'
 
-import { QueryClient, QueryClientProvider } from 'react-query'
+import { QueryClientProvider } from 'react-query'
 import { makeServer } from '../services/mirage'
+import { queryClient } from '../services/queryClient'
 
 if (process.env.NODE_ENV == 'development') {
 	makeServer();
 }
 
-const queryClient = new QueryClient()
+
 
 function MyApp({ Component, pageProps }: AppProps ) {
   return (
