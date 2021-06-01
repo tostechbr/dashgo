e7224164545563783d9364549e63c299a968c172
Tiago Oliveira <tiagolivesan@outlook.com>
2021-05-31 21:37:36 -0300

    dashgo

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -27,9 +27,7 @@ import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
   const [page, setPage] = useState(1);
-  const { data, isLoading, error, isFetching } = useUsers()
-
-  console.log(page);
+  const { data, isLoading, error, isFetching } = useUsers(page)
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -85,7 +83,7 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {data.map((user) => {
+                  {data.users.map((user) => {
                     return (
                       <Tr key={user.id}>
                         <Td px={["4", "4", "6"]}>
@@ -107,9 +105,9 @@ export default function UserList() {
               </Table>
 
               <Pagination 
-                totalCountOfRegisters={200}
-                currentPage={5}
-                onPageChange={() => {}}
+                totalCountOfRegisters={data.totalCount}
+                currentPage={page}
+                onPageChange={setPage}
               />
             </>
           )}
--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -2,33 +2,47 @@ import { useQuery } from "react-query";
 import { api } from "../api";
 
 type User = {
-    id: string;
-    name: string;
-    email: string;
-    createdAt: string;
+  id: string;
+  name: string;
+  email: string;
+  createdAt: string;
 }
 
-export async function getUsers():Promise<User[]>{
-    const { data } = await api.get("users");
+type GetUsersResponse = {
+  totalCount: number;
+  users: User[];
+}
 
-    const users = data.users.map((user) => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
-          day: "2-digit",
-          month: "long",
-          year: "numeric",
-        }),
-      };
-    });
+export async function getUsers(page: number): Promise<GetUsersResponse> {
+  const { data, headers } = await api.get("users", {
+    params: {
+      page,
+    }
+  })
 
-    return users;
+  const totalCount = Number(headers['x-total-count'])
+
+  const users = data.users.map((user) => {
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }),
+    };
+  });
+
+  return {
+    users,
+    totalCount,
   }
+}
 
-export function useUsers() {
-    return useQuery("users", getUsers, {
-        staleTime: 1000 * 5,
-      });
+export function useUsers(page: number) {
+  return useQuery(["users", page], () => getUsers(page), {
+    staleTime: 1000 * 5,
+  });
 }
